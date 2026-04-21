a730d2a37abe0a99ee1fedace5cc09bf02fe797c
Sites Newwork <sitesnewwork@gmail.com>
2026-04-21 04:05:48 +0300

    fix: import admin mobile stability CSS

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
+import "./admin-mobile-fix.css";
 import "./i18n";
 import { detectAndApplyLanguage } from "./i18n/geoDetect";
 
